776e53d151804e4ba5cfb603f1cebc3a851852c1
lagangole <164272321+lagangole@users.noreply.github.com>
2024-08-08 23:41:39 +0000

    instead on combination I created two pages

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from os import name, read
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import sqlite3
 from sqlite3 import Error
 
@@ -23,20 +22,31 @@ def create_connection(db_file):
 def render_home():
     return render_template("index.html")
 
-@app.route('/titles/<title_read>')
-def render_webtitle(title_read):
-    query = "SELECT title, description FROM books WHERE read =?"
-    name = title_read
+@app.route('/read')
+def render_read():
+    query = "SELECT title, description, published_year FROM books WHERE read = 1"
     con = create_connection(DATABASE)
-
-
+    print(con)
     cur = con.cursor()
     #Query the database
-    cur.execute(query, (name,))
-    title_list = cur.fetchall()
+    cur.execute(query)
+    book_list = cur.fetchall()
     con.close()
-    print(title_list)
-    return render_template("read.html", titles=title_list)
+    print(book_list)
+    return render_template("read.html", books=book_list)
+
+
+@app.route('/not_read')
+def render_notread():
+    query = "SELECT title, author, description, published_year FROM books WHERE read = 0"
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+    #Query the database
+    cur.execute(query)
+    book_list = cur.fetchall()
+    con.close()
+    print(book_list)
+    return render_template("not_read.html", books=book_list )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81)
