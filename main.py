d5a484df6dc51f2e91ec00f316071224c9cfa7ca
lagangole <164272321+lagangole@users.noreply.github.com>
2024-08-08 21:53:40 +0000

    Friday/9

--- main.py
+++ main.py
@@ -19,100 +19,24 @@ def create_connection(db_file):
         print(e)
     return None
 
-
-def get_titles(title_read):
-    """Returns titles and description depending on if it is read"""
-    name = int(title_read)
-    if name == 1:
-        name = "read"
-    elif name == 0:
-        name = "not read"
-    query = "SELECT title, description FROM books WHERE read=?"
-    con = create_connection(DATABASE)
-    cur = con.cursor()
-
-    # Query the DATABASE
-    cur.execute(query, (name,))
-    read_list = cur.fetchall()
-    
-    con.close()
-    #print(tag_list)
-    return read_list
-
-def get_reads():
-    """ Returns the items 1 and 0"""
-    con = create_connection(DATABASE)
-    query = "SELECT DISTINCT read FROM books"
-    #records = run_query(query)
-    cur = con.cursor()
-    cur.execute(query)
-    records = cur.fetchall()
-    #print(records)
-    for i in range(len(records)):
-        records[i] = records[i][0]
-    #print(records)
-    return records
-
 @app.route('/')
 def render_home():
-    return render_template('index.html', reads=get_reads())
-
+    return render_template("index.html")
 
 @app.route('/titles/<title_read>')
-def render_webpage(title_read):
-
-    name = int(title_read)
-    if name == 1:
-        name = "read"
-    elif name == 0:
-        name = "not read"
-    print(name)
-    return render_template('libray.html', titles=get_titles(title_read), name=name, reads=get_reads())
-
-@app.route('/sort/<name>')
-def render_sortpage(name):
-    sort = request.args.get('sort', 'title')
-    order = request.args.get('order', 'asc')  # Get the current sort order, default to 'asc' if not provided
-
-    # Toggle the sort order
-    if order == 'asc':
-        new_order = 'desc'
-    else:
-        new_order = 'asc'
-
-    # Sort query
-    query = "SELECT title, description FROM books WHERE title=? ORDER BY " + sort + " " + order
-
+def render_webtitle(title_read):
+    query = "SELECT title, description FROM books WHERE read =?"
+    name = title_read
     con = create_connection(DATABASE)
-    cur = con.cursor()
 
-    # Query the DATABASE
+
+    cur = con.cursor()
+    #Query the database
     cur.execute(query, (name,))
     title_list = cur.fetchall()
     con.close()
-
-    return render_template('library.html', titles=title_list, name=name, reads=get_reads(), order=new_order)
-
-
-@app.route('/search', methods=['GET', 'POST'])
-def render_search():
-    """
-    Find all records which contain the search item
-    :POST contains the search value
-    :returns a rendered page
-    """
-    search = request.form['search']
-    name = "Search for " + search
-    query = "SELECT title, description FROM books WHERE " \
-            "title LIKE ? OR description LIKE ?"
-    search = "%" + search + "%"
-    con = create_connection(DATABASE)
-    cur = con.cursor()
-    cur.execute(query, (search, search))
-    title_list = cur.fetchall()
-    con.close()
-
-    return render_template("library.html", titles=title_list, name=name, reads=get_reads())
+    print(title_list)
+    return render_template("read.html", titles=title_list)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81)
