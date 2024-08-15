aff347d091e264b23d0fea2ae97d920c0cd2e1be
lagangole <164272321+lagangole@users.noreply.github.com>
2024-08-15 21:45:29 +0000

    combining pages

--- main.py
+++ main.py
@@ -22,31 +22,25 @@ def create_connection(db_file):
 def render_home():
     return render_template("index.html")
 
-@app.route('/read')
-def render_read():
-    query = "SELECT title, author, description, published_year FROM books WHERE read = 1"
+@app.route('/read/<read_status>')
+def render_webpage(read_status):
+    status = int(read_status)
+    query = "SELECT title, author, description, published_year FROM books WHERE read = ?"
     con = create_connection(DATABASE)
     print(con)
     cur = con.cursor()
     #Query the database
-    cur.execute(query)
+    cur.execute(query, (status,))
     book_list = cur.fetchall()
     con.close()
     print(book_list)
-    return render_template("read.html", books=book_list)
+    status_title = status
+    if status_title == 1:
+        status_title = "Read"
+    elif status_title == 0:
+        status_title = "Not Read"
+    return render_template("webbooks.html", books=book_list, status=status_title)
 
-
-@app.route('/not_read')
-def render_notread():
-    query = "SELECT title, author, description, published_year FROM books WHERE read = 0"
-    con = create_connection(DATABASE)
-    cur = con.cursor()
-    #Query the database
-    cur.execute(query)
-    book_list = cur.fetchall()
-    con.close()
-    print(book_list)
-    return render_template("not_read.html", books=book_list )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81)
